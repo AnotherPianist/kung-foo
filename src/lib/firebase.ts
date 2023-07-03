461efea449be5d238af17ad072358aeb3a52d000
ThePianist <cafarimo@outlook.com>
2023-07-03 12:27:30 -0400

    Add real time firebase data store

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,9 +1,8 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import type { User } from "firebase/auth";
+import { doc, getFirestore, onSnapshot } from "firebase/firestore";
+import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-import { writable } from "svelte/store";
+import { derived, type Readable, writable } from "svelte/store";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAO-MyvNLyYw5zwI6Qd3I1pqOcU_tb6gdM",
@@ -37,4 +36,36 @@ function userStore() {
   return { subscribe };
 }
 
+export function docStore<T>(path: string) {
+  let unsubscribe: () => void;
+
+  const docRef = doc(db, path);
+
+  const { subscribe } = writable<T | null>(null, set => {
+    unsubscribe = onSnapshot(docRef, snapshot => set((snapshot.data() as T) ?? null));
+
+    return () => unsubscribe();
+  });
+
+  return {
+    subscribe,
+    ref: docRef,
+    id: docRef.id
+  };
+}
+
 export const user = userStore();
+
+interface UserData {
+  username: string;
+  bio: string;
+  photoURL: string;
+  links: any[];
+}
+
+export const userData: Readable<UserData | null> = derived(user, ($user, set) => {
+  if ($user)
+    return docStore(`users/${$user.uid}`).subscribe(set);
+  else
+    set(null);
+});
